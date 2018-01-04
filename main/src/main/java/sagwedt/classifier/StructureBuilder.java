75226308493ff62a5a24c43f1032ee7a4936dce0
tomalla5120x <tomalla5120x@gmail.com>
2018-01-04 14:17:31 +0100

    StructureBuilder - implementacja limitu słów
    Został zaimplementowany limit struktury. Teraz można ograniczać
    wielkość wygenerowanej struktury metodą setLimit.

--- main/src/main/java/sagwedt/classifier/StructureBuilder.java
+++ main/src/main/java/sagwedt/classifier/StructureBuilder.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 import weka.core.Attribute;
 import weka.core.Instance;
@@ -20,16 +21,24 @@ import weka.core.Instances;
 public class StructureBuilder
 {
 	private TreeMap<String, Integer> m_wordCounts;
-	//private int m_wordLimit;
+	private int m_wordLimit;
+	private Instances m_calculatedDataset;
 	
 	public static final String NO = "no";
 	public static final String YES = "yes";
 	public static final double VAL_NO = 0.0;
 	public static final double VAL_YES = 1.0;
 	
+	/**
+	 * Tworzy StructureBuilder z domyślnymi wartościami.<br>
+	 * <br>
+	 * Domyślnie jest brak ograniczeń na wielkość struktury.
+	 */
 	public StructureBuilder()
 	{
 		m_wordCounts = new TreeMap<String, Integer>();
+		m_wordLimit = Integer.MAX_VALUE;
+		m_calculatedDataset = null;
 	}
 	
 	/**
@@ -38,6 +47,8 @@ public class StructureBuilder
 	 */
 	public void add(Instance instance)
 	{
+		m_calculatedDataset = null;
+		
 		for(int i=0; i<instance.dataset().numAttributes(); ++i)
 		{
 			Attribute att = instance.dataset().attribute(i);
@@ -57,12 +68,13 @@ public class StructureBuilder
 	 */
 	public void setWordLimit(int limit)
 	{
-		throw new UnsupportedOperationException("not implemented yet");
+		if(limit < 1)
+			limit = Integer.MAX_VALUE;
 		
-		/*if(limit < 1)
-			m_wordLimit = Integer.MAX_VALUE;
-		else
-			m_wordLimit = limit;*/
+		if(limit != m_wordLimit && m_calculatedDataset != null && limit < m_calculatedDataset.numAttributes())
+			m_calculatedDataset = null;
+		
+		m_wordLimit = limit;
 	}
 	
 	/**
@@ -71,41 +83,83 @@ public class StructureBuilder
 	 */
 	public TrainingSet generateEmptyTrainingSet()
 	{
-		//TODO: tymczasowo jest tworzona struktura z nieograniczoną liczbą słów
+		if(m_calculatedDataset != null)
+			return new TrainingSet(m_calculatedDataset);
+		
+		TreeMap<String, Integer> destDict;
+		
+		if(m_wordLimit >= m_wordCounts.size())
+			destDict = m_wordCounts;
+		else
+		{
+			/*
+			
+			Example: limit = 3
+			a - 4, b - 10, c - 1, d - 4, e - 6, f - 80
+			
+			f - 80, b - 10, e - 6, d - 4, a - 4, c - 1 // sort by frequency
+			f - 80, b - 10, e - 6                      // truncate
+			b - 10, e - 6, f - 80                      // sort by word
+			
+			*/
+			
+			class SortPair implements Comparable<SortPair>
+			{
+				public int count;
+				public String word;
+				
+				public SortPair(int _count, String _word)
+				{
+					count = _count;
+					word = _word;
+				}
+				
+				public int compareTo(SortPair pair)
+				{
+					if(count == pair.count)
+						return word.compareTo(pair.word);
+					return Integer.compare(count, pair.count);
+				}
+			}
+			
+			TreeSet<SortPair> sortSet = new TreeSet<SortPair>();
+			SortPair smallest = null;
+			
+			for(Map.Entry<String, Integer> entry: m_wordCounts.entrySet())
+			{
+				SortPair pair = new SortPair(entry.getValue(), entry.getKey());
+				
+				if(sortSet.size() < m_wordLimit)
+				{
+					sortSet.add(pair);
+					
+					if(smallest == null || pair.compareTo(smallest) < 0)
+						smallest = pair;
+				} else if(smallest.compareTo(pair) < 0)
+				{
+					sortSet.remove(smallest);
+					sortSet.add(pair);
+					smallest = sortSet.first();
+				}
+			}
+			
+			// ----
+			
+			destDict = new TreeMap<String, Integer>();
+			
+			for(SortPair pair: sortSet)
+				destDict.put(pair.word, pair.count);
+		}
+		
+		// --------------
 		
 		ArrayList<Attribute> attList = new ArrayList<Attribute>();
 		attList.add(new Attribute("positive", Arrays.asList(NO, YES)));
 		
-		for(Map.Entry<String, Integer> entry: m_wordCounts.entrySet())
+		for(Map.Entry<String, Integer> entry: destDict.entrySet())
 			attList.add(new Attribute(entry.getKey(), false));
 		
-    	return new TrainingSet(new Instances("structure", attList, 0));
-		
-		/*
-		class VSort implements Comparator<Map.Entry<String, Integer>> {
-			public int compare(Map.Entry<String, Integer> one, Map.Entry<String, Integer> two) {
-				return two.getValue() - one.getValue(); // smallest values first
-			}
-		}
-		
-		VSort comp = new VSort();
-		Map.Entry<String, Integer> smallestInVector = null;
-		SortedSet<Map.Entry<String, Integer>> set = new TreeSet<Map.Entry<String, Integer>>(comp);
-		//  5 10 100 150     
-		//
-		for(Map.Entry<String, Integer> entry: m_wordCounts.entrySet())
-		{
-			if(set.size() < m_wordLimit)
-			{
-				set.add(entry);
-				
-				if(smallestInVector == null)
-					smallestInVector = entry;
-				else
-					smallestInVector = Math.min(smallestInVector, entry.getValue());
-			} else if(smallestInVector < entry.getValue())
-				set.remove(arg0);
-		}
-		*/
+		m_calculatedDataset = new Instances("structure", attList, 0);
+    	return new TrainingSet(m_calculatedDataset);
 	}
 }
