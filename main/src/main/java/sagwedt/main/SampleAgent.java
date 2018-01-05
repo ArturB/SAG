8712d6fd9d2b133b226ac238ad35479377bbe265
Artur Brodzki <artur.brodzki@gmail.com>
2018-01-05 22:55:35 +0100

    pom libraries

--- main/src/main/java/sagwedt/main/SampleAgent.java
+++ main/src/main/java/sagwedt/main/SampleAgent.java
@@ -1,16 +1,3 @@
-package sagwedt.main;
+class SampleAgent {
 
-import jade.core.*;
-
-public class SampleAgent extends Agent {
- 
-  /**
-	 * 
-	 */
-	private static final long serialVersionUID = 55182624475616215L;
-
-	protected void setup() {
-		// Printout a welcome message
-		System.out.println("Hello! Is sample agent alive?: " + this.isAlive() );
-	}
 }
